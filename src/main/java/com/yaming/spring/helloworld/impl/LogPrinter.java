0b476a32334d5546dce4d0d9d998c51a55d8d9b1
YaMing Wu <wuyaming@gmail.com>
2018-11-21 11:12:04 +0800

    add AOP LogPrinter

---
+++ src/main/java/com/yaming/spring/helloworld/impl/LogPrinter.java
@@ -0,0 +1,19 @@
+package com.yaming.spring.helloworld.impl;
+
+import java.io.PrintStream;
+
+public class LogPrinter {
+    private PrintStream stream;
+
+    public LogPrinter(PrintStream stream) {
+        this.stream = stream;
+    }
+
+    public void printBeforeSayHello() {
+        stream.println("Fa la la, before hello");
+    }
+
+    public void printAfterSayHello() {
+        stream.println("Tee hee hee, after hello");
+    }
+}
