0cd4e47905e31f9b6717504b4b19670ffbefe2df
YaMing Wu <wuyaming@gmail.com>
2018-12-09 17:31:34 -0600

    start spring mvc

---
+++ src/main/java/ch04/concert/EncorableIntroducer.java
@@ -0,0 +1,13 @@
+package ch04.concert;
+
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.DeclareParents;
+
+@Aspect
+public class EncorableIntroducer {
+//    @DeclareParents(
+//            value="ch04.consert.Performance+",
+//            defaultImpl = DefaultEncoreable.class)
+//
+//    public static Encoreable encoreable;
+}
---
+++ src/main/java/ch04/concert/Encoreable.java
@@ -0,0 +1,5 @@
+package ch04.concert;
+
+public interface Encoreable {
+    void performEncore();
+}
