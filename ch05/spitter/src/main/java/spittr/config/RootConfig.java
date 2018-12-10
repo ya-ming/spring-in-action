d0b1aa12438017c4e267818c7440629a22778a86
YaMing Wu <wuyaming@gmail.com>
2018-12-10 17:15:17 -0600

    spittles, create controller to show the view, not finished

---
+++ ch05/spitter/src/main/java/spittr/config/RootConfig.java
@@ -0,0 +1,19 @@
+package spittr.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.FilterType;
+import org.springframework.web.servlet.ViewResolver;
+import org.springframework.web.servlet.config.annotation.DefaultServletHandlerConfigurer;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+
+@Configuration
+@ComponentScan(basePackages={"spittr"},
+        excludeFilters = {
+        @ComponentScan.Filter(type= FilterType.ANNOTATION, value=EnableWebMvc.class)
+})
+public class RootConfig {
+}
