40f7ed90c229648d08a3b16ff1a15f10df01a799
YaMing Wu <wuyaming@gmail.com>
2018-12-09 19:13:51 -0600

    project init

--- ch05/spitter/src/main/java/spittr/config/WebConfig.java
+++ ch05/spitter/src/main/java/spittr/config/WebConfig.java
@@ -1,4 +1,4 @@
-package ch05.spitter.config;
+package spittr.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
